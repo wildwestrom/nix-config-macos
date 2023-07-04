65ae53c1d105e8a3854e48a436351e20feedbed0
Christian Westrom <c.westrom@westrom.xyz>
2023-07-04 18:32:12 +0900

    whitespace changes

--- flake.nix
+++ flake.nix
@@ -1,43 +1,47 @@
 {
-	description = "West's MacOS Config";
+  description = "West's MacOS Config";
 
-	inputs = {
-		nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-		home-manager = {
-			url = "github:nix-community/home-manager/master";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-		darwin = {
-			url = "github:lnl7/nix-darwin";
-			inputs.nixpkgs.follows = "nixpkgs";
-		};
-
-	};
-	outputs = { nixpkgs, home-manager, darwin, ... }@inputs: {
-		darwinConfigurations = {
-			"Mains-MacBook-Pro" = darwin.lib.darwinSystem {
-				system = "x86_64-darwin";
-				inherit inputs;
-				pkgs = import nixpkgs {
-					system = "x86_64-darwin";
-					config = {
-						allowUnfree = true;
-					};
-				};
-				modules = [
-					./modules/darwin
-					home-manager.darwinModules.home-manager
-					{
-						home-manager = {
-							useGlobalPkgs = true;
-							useUserPackages = true;
-							users.main.imports = [ ./modules/home-manager ];
-						};
-					}
-				];
-			};
-		};
-	};
+    darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    home-manager,
+    darwin,
+    ...
+  } @ inputs: {
+    darwinConfigurations = {
+      "Mains-MacBook-Pro" = darwin.lib.darwinSystem {
+        system = "x86_64-darwin";
+        inherit inputs;
+        pkgs = import nixpkgs {
+          system = "x86_64-darwin";
+          config = {
+            allowUnfree = true;
+          };
+        };
+        modules = [
+          ./modules/darwin
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.main.imports = [./modules/home-manager];
+            };
+          }
+        ];
+      };
+    };
+  };
 }
--- modules/home-manager/gpg-agent.conf.nix
+++ modules/home-manager/gpg-agent.conf.nix
@@ -1,15 +1,13 @@
-{ pkgs }:
+{pkgs}: ''
+  # TTL
+  default-cache-ttl 60
+  max-cache-ttl 120
 
-''
-# TTL
-default-cache-ttl 60
-max-cache-ttl 120
+  # SSH
+  enable-ssh-support
 
-# SSH
-enable-ssh-support
-
-# Pinentry config
-allow-emacs-pinentry
-allow-loopback-pinentry
-pinentry-program ${pkgs.pinentry_mac}/bin/pinentry
+  # Pinentry config
+  allow-emacs-pinentry
+  allow-loopback-pinentry
+  pinentry-program ${pkgs.pinentry_mac}/bin/pinentry
 ''
